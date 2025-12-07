cd19ecc94b01d2dec9d430979538279bb4161352
Emard <vordah@gmail.com>
2025-12-07 17:39:59 +0100

    use run()

--- examples/one_axis_variablespeedtrack.py
+++ examples/one_axis_variablespeedtrack.py
@@ -3,24 +3,26 @@ import time
 '''One axis variable speed track example'''
 smc=synscan.motors()
 
-AXIS=2
-#FORWARD
-#RAMP UP
-for speed in range(0,50,1):
+def run():
+  AXIS=2
+  #FORWARD
+  #RAMP UP
+  for speed in range(0,50,1):
     time.sleep(.1)
     smc.axis_track(AXIS,speed/10)
-#RAMP DOWN
-for speed in range(50,0,-1):
+  #RAMP DOWN
+  for speed in range(50,0,-1):
     time.sleep(.1)
     smc.axis_track(AXIS,speed/10)
 
-#BACKWARDS
-#RAMP UP
-for speed in range(0,50,1):
+  #BACKWARDS
+  #RAMP UP
+  for speed in range(0,50,1):
     time.sleep(.1)
     smc.axis_track(AXIS,-speed/10)
-#RAMP DOWN
-for speed in range(50,0,-1):
+  #RAMP DOWN
+  for speed in range(50,0,-1):
     time.sleep(.1)
     smc.axis_track(AXIS,-speed/10)
 
+run()
